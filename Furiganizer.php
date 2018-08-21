ad2ac9317ef11b225e9f52fc2fc4cfd8ccbef4b9
Shige <itoshige1017@gmail.com>
2018-08-22 00:07:12 +0900

    commit

--- Furiganizer.php
+++ Furiganizer.php
@@ -7,49 +7,54 @@ define('DELIMITER', ',');
  */
 class Furiganizer {
     private $igo;
-
+    
     public function __construct() {
         $this->igo = new Igo(__DIR__ ."/ipadic", "UTF-8");
     }
-
+    
     /**
-     * 姓名分割されていない漢字氏名をkuromojiを使用して分割、予測したフリガナを返却する
+     * 姓名分割されていない漢字氏名をigo-phpを使用して分割、予測したフリガナを返却する
      */
     public function furiganize($kanjiname) {
-
+        
         if($this->isEmpty($kanjiname))
             return '';
-
+        
         // 全角スペースを半角スペースへ変換してtrim
         $name = trim(mb_convert_kana($kanjiname, 's', 'UTF-8'));
 
         // 品詞情報なども合わせて取得する
         $result = $this->igo->parse($name);
-
+        
         $kanji = $kana = '';
-
+        
         foreach($result as $seimei) {
             if($this->isEmpty($seimei))
                 continue;
-
+                
             // 漢字
             $kanji .= $seimei->surface . DELIMITER;
-
+            
             // フリガナ
             $features = $seimei->feature;
+            
+            
+            $furigana = rtrim($seimei->surface, ',');
 
-
-            $furigana = isset(explode(',', $features)[7]) ? explode(',', $features)[7] : rtrim($kanji, ',');
-
+            // 漢字に英数字が含まれていなかった場合
+            if (!preg_match("/[!-~]/", $seimei->feature)) {
+                $furigana = isset(explode(',', $features)[7]) ? explode(',', $features)[7] : rtrim($seimei->surface, ',');
+            }
+                
             $kana .= $furigana . DELIMITER;
         }
-
+        
         return rtrim($kanji, ',') . ':' . rtrim($kana, ',');
     }
 
     /**
      * null、半角または全角スペースか判定する
-     *
+     * 
      * @param $value
      * @return boolean
      */
